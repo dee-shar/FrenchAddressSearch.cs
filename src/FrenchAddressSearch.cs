1e4e205cce0d60b4f38378499388ab12817c24d4
huok <77536370+dee-shar@users.noreply.github.com>
2025-10-25 19:28:22 +0300

    Update and rename french_address_search.sh to FrenchAddressSearch.cs

---
+++ src/FrenchAddressSearch.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace FrenchAddressSearchApi
+{
+    public class FrenchAddressSearch
+    {
+        private readonly HttpClient httpClient;
+        private readonly string apiUrl = "https://geo.api.gouv.fr";
+        public FrenchAddressSearch()
+        {
+            httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.UserAgent.ParseAdd(
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36");
+            httpClient.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<string> searchAddress(string parameters)
+        {
+            var response = await httpClient.GetAsync($"{apiUrl}/search/{parameters}");
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public async Task<string> getRegions()
+        {
+            var response = await httpClient.GetAsync($"{apiUrl}/regions");
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public async Task<string> getCommunes()
+        {
+            var response = await httpClient.GetAsync($"{apiUrl}/communes_associees_deleguees");
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+        public async Task<string> getDepartaments()
+        {
+            var response = await httpClient.GetAsync($"{apiUrl}/departements");
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
